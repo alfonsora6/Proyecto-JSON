184844e17230bf1259747548dc3e28b56382c81c
Alfonso Roldán <megaalfonso84@gmail.com>
2022-03-01 20:39:26 +0100

    Ejercicios 1,2,3 y 4 realizados

--- funciones.py
+++ funciones.py
@@ -17,12 +17,13 @@ def menu():
     3- Ver el reparto de una película.
     4- Filtrar películas por género.
     5- Mostrar géneros de una película (Se debe especificar el cine).
+    6- Salir
     '''
     print(menu)
     opcion=int(input("Selecciona una opción: "))
-    while opcion<1 or opcion>5:
-        print("Error, el número de la opción debe estar comprendido entre el 1 y el 5")
-        opcion=int(input("Selecciona una opción: "))
+    while opcion<1 or opcion>6:
+        print("Error, el número de la opción debe estar comprendido entre el 1 y el 6")
+        opcion=int(input("\nSelecciona una opción: "))
     return opcion
 
 #Ejercicio 1
@@ -42,3 +43,41 @@ def contar_sesiones(datos):
         cines.append(cine)
         sesiones.append(len(sesion.get("sesiones")))
     return cines,sesiones
+
+#Ejercicio 3
+def mostar_reparto(datos,pelicula):
+    reparto=[]
+    if pelicula in datos.get("peliculas"):
+        for actor in datos.get("peliculas").get(pelicula).get("reparto"):
+            reparto.append(actor)
+        return reparto
+    else:
+        print("No hay ninguna película con el nombre indicado.")
+        reparto=False
+        return reparto
+        
+#Ejercicio 4
+def mostrar_generos(datos):
+    lista=[]
+    for pelicula in datos.get("peliculas"):
+        for genero in datos.get("peliculas").get(pelicula).get("genero"):
+            if genero not in lista:
+                lista.append(genero)
+    return lista
+
+def mostrar_peliculas_por_genero(datos,genero):
+    lista=[]
+    a=False
+    peliculas=list(datos.get("peliculas").keys())
+
+    for pelicula in peliculas:
+        if genero in datos.get("peliculas").get(pelicula).get("genero"):
+            lista.append(pelicula)
+            a=True
+    
+    if a==False:
+        lista=False
+        print("No hay ningún género con el nombre indicado.")
+    return lista
+
+#Ejercicio 5
--- programa.py
+++ programa.py
@@ -2,7 +2,7 @@ from funciones import *
 fichero=leer_json("cine.json")
 
 respuesta=menu()
-while respuesta!=5:
+while respuesta!=6:
     
     #Ejercicio 1
     if respuesta==1:
@@ -15,9 +15,33 @@ while respuesta!=5:
         cines,sesiones=contar_sesiones(fichero)
         for cine,sesion in zip(cines,sesiones):
             print("\nCine:",cine,"\nNúmero de sesiones:",sesion)
-    #elif respuesta==3:
-    #elif respuesta==4:
+    
+    #Ejercicio 3
+    elif respuesta==3:
+        pelicula=input("Introduce una película: ")
+        reparto=mostar_reparto(fichero,pelicula)
+        if reparto != False:    
+            print("\nReparto de %s:\n"%pelicula)
+            for actor in reparto:
+                print("-",actor)
+
+    #Ejercicio 4
+    elif respuesta==4:
+        generos=mostrar_generos(fichero)
+        print("\nGÉNEROS:")
+        for gen in generos:
+            print("-",gen)
+        print("")
+        genero=input("Introduce un género: ")
+        peliculas=mostrar_peliculas_por_genero(fichero,genero)
+        if peliculas != False:
+            print("\nPelículas que pertenecen al género %s:\n"%genero)
+            for pelicula in peliculas:
+                print("-",pelicula)
+            
     #elif respuesta==5:
 
+
     respuesta=menu()
-    
+
+print("Has salido del programa.")
